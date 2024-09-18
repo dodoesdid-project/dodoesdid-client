5abf3fec492738da810040615f0f0b87bd9eb85d
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-09-18 20:13:39 +0900

    feat: GUI가이드라인설정 -#2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,40 @@
 module.exports = {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      desktop: '375px',
+    },
+    colors: {
+      primary: {
+        100: '#99cdff',
+        200: '#7bafff',
+        300: '#679bff',
+        400: '#5387ff',
+        500: '#3f73f7',
+        DEFAULT: '#3f73f7',
+        600: '#1457ff',
+        700: '#245be8',
+      },
+      sub: {
+        100: '#ffd398',
+        200: '#ffbf84',
+        300: '#ffab70',
+        400: '#ff8d52',
+        500: '#ff6d2d',
+      },
+      gray: {
+        30: '#f3f3f3',
+        40: '#e9e9e9',
+        50: '#d5d5d5',
+        60: '#c0c0c0',
+        70: '#989898',
+        80: '#707070',
+        90: '#525252',
+        100: '#202020',
+      },
+      black: '#000000',
+      white: '#ffffff',
+    },
     extend: {},
   },
   plugins: [],
