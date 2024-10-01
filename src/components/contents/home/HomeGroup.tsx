1689cb8a3570547526210137182467603a2c2259
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-10-01 17:27:06 +0900

    feat: 그룹생성, 프로필x그룹x,프로필o,그룹x -#21

--- src/components/contents/home/HomeGroup.tsx
+++ src/components/contents/home/HomeGroup.tsx
@@ -7,7 +7,7 @@ const HomeGroup = () => {
         <img src=" http://via.placeholder.com/640x480" alt="그룹이미지" />
       </div>
       <p className="text-gray-100 text-[11px] text-center dark:text-gray-30">
-        다이어트하기
+        다이어트 하기
       </p>
     </div>
   );
