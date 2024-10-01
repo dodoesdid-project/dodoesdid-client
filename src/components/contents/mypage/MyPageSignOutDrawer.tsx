c772d64cffb080b299a5e3ea82294eeb657a6c49
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-10-01 14:49:48 +0900

    feat: 마이페이지UI -#23

---
+++ src/components/contents/mypage/MyPageSignOutDrawer.tsx
@@ -0,0 +1,61 @@
+import useDarkMode from '@lib/hooks/useDarkMode';
+
+import Button from '@components/common/Button';
+
+import { ReactComponent as CloseDarkButton } from '@assets/images/common/close-white.svg';
+import { ReactComponent as CloseButton } from '@assets/images/common/close.svg';
+
+import { Drawer } from 'antd';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type Props = { onClose: () => void };
+
+const MyPageSignOutDrawer = ({ onClose }: Props) => {
+  const navigate = useNavigate();
+  const isDarkMode = useDarkMode();
+
+  return (
+    <Drawer
+      placement="bottom"
+      closable={false}
+      onClose={onClose}
+      open
+      className="dark:bg-[#2a2a2a]"
+    >
+      <div className="flex flex-col">
+        {isDarkMode ? (
+          <CloseDarkButton
+            onClick={onClose}
+            className="cursor-pointer mb-[24px] self-end"
+          />
+        ) : (
+          <CloseButton
+            onClick={onClose}
+            className="cursor-pointer mb-[24px] self-end"
+          />
+        )}
+        <p className="text-gray-100 font-semibold text-[16px] mb-[16px] text-center dark:text-gray-30">
+          정말 탈퇴하시겠어요?
+        </p>
+        <p className="text-gray-90 text-[14px] mb-[20px] px-[10px] text-center dark:text-gray-40">
+          탈퇴하실 경우 두더지에서 작성한 모든 글과 활동 내역이 삭제돼요. 삭제된
+          정보는 다시 복구할 수 없으므로 신중하게 결정해주세요.
+        </p>
+        <Button
+          buttonType="tinted-semibold"
+          name="아니요"
+          style={{ marginBottom: '10px' }}
+          onClick={onClose}
+        />
+        <Button
+          buttonType="fill-semibold"
+          name="예, 탈퇴하겠습니다"
+          onClick={() => navigate('/login')}
+        />
+      </div>
+    </Drawer>
+  );
+};
+
+export default MyPageSignOutDrawer;
--- src/routes/routing.tsx
+++ src/routes/routing.tsx
@@ -1,12 +1,22 @@
 import DodoesdidPage from '@pages/DodoesdidPage';
 import FeedPage from '@pages/FeedPage';
 import Homepage from '@pages/Homepage';
-import Mypage from '@pages/Mypage';
 import JoinPage from '@pages/join';
 import LoginPage from '@pages/login';
 import LoginEmailPage from '@pages/login/email';
 import LoginSearchIdPage from '@pages/login/search-id';
 import LoginSearchPwPage from '@pages/login/search-pw';
+import Mypage from '@pages/mypage';
+import AccountManagePage from '@pages/mypage/account';
+import MyGroupPage from '@pages/mypage/group';
+import MyGroupInfoPage from '@pages/mypage/group-info';
+import ChangeGroupNicknamePage from '@pages/mypage/group-nickname';
+import ChangeNicknamePage from '@pages/mypage/nickname';
+import ChangePasswordPage from '@pages/mypage/password';
+import ChangePhonePage from '@pages/mypage/phone';
+import ChangeProfilePage from '@pages/mypage/profile';
+import SignOutPage from '@pages/mypage/signout';
+import SignOutAgreePage from '@pages/mypage/signout-agree';
 
 import DefaultLayout from '@components/common/base/DefaultLayout';
 import NoNavLayout from '@components/common/base/NoNavLayout';
@@ -26,6 +36,17 @@ const router = createBrowserRouter([
           { path: '/feed', element: <FeedPage /> },
           { path: '/dodoesdid', element: <DodoesdidPage /> },
           { path: '/mypage', element: <Mypage /> },
+          { path: '/mypage/profile', element: <ChangeProfilePage /> },
+          { path: '/mypage/profile/nickname', element: <ChangeNicknamePage /> },
+          { path: '/mypage/account', element: <AccountManagePage /> },
+          { path: '/mypage/account/password', element: <ChangePasswordPage /> },
+          { path: '/mypage/account/phone', element: <ChangePhonePage /> },
+          { path: '/mypage/group', element: <MyGroupPage /> },
+          { path: '/mypage/group/info', element: <MyGroupInfoPage /> },
+          {
+            path: '/mypage/group/info/nickname',
+            element: <ChangeGroupNicknamePage />,
+          },
         ],
       },
     ],
@@ -38,6 +59,8 @@ const router = createBrowserRouter([
       { path: '/login-email', element: <LoginEmailPage /> },
       { path: '/login-email/search/id', element: <LoginSearchIdPage /> },
       { path: '/login-email/search/pw', element: <LoginSearchPwPage /> },
+      { path: '/mypage/account/signout', element: <SignOutPage /> },
+      { path: '/mypage/account/signout-agree', element: <SignOutAgreePage /> },
     ],
   },
 ]);
