78ce7a9377aacadde95a0c4c7d6e67df8aa0e2aa
정찬영 <q135794268020@gmail.com>
2024-10-01 14:55:20 +0900

    feat: feedDatail 내부 이모지 구현 -#22

---
+++ src/components/contents/Feed/Emoji/EmojiGroup.tsx
@@ -0,0 +1,12 @@
+import EmojiItem from './EmojiItem';
+
+const EmojiGroup = () => {
+  return (
+    <>
+      <div className="overflow-x-auto ">
+        <EmojiItem />
+      </div>
+    </>
+  );
+};
+export default EmojiGroup;
