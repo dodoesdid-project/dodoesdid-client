c3b6b81c993899497a66f2cbccaf777f70cc1a53
정찬영 <q135794268020@gmail.com>
2024-10-04 08:46:32 +0900

    feat: 피드 및 피드 상세, 액션 버튼 타입, Axios 코드 작성 -#52

--- src/types/feedType.ts
+++ src/types/feedType.ts
@@ -1,16 +1,71 @@
-export interface IFeedItem {
-  userName: string;
-  userProfileImageUrl: string;
-  dazimContent: string;
-  dazimContentId: string;
-  dazimFileUrl: string;
-  dazimTimeAgo: string;
-  dazimLikeCount: number;
-  totalCommentAndReplyCount: number;
+export interface IUserProfile {
+  nickName: string;
+  thumbnail: string;
 }
 
-export interface IFeedApiResponse {
-  code: number;
-  message: string;
-  data: IFeedItem[];
+export interface IUser {
+  id: string;
+  profile: IUserProfile | null;
+}
+
+export interface IFeed {
+  id: string;
+  groupId: string;
+  content: string;
+  photo?: string;
+  createAt: string;
+  updateAt: string;
+  user: IUser;
+
+  fireCount?: number;
+  starCount?: number;
+  congratulationsCount?: number;
+  heartCount?: number;
+  musicCount?: number;
+  isMeReactionFire?: boolean;
+  isMeReactionStar?: boolean;
+  isMeReactionCongratulations?: boolean;
+  isMeReactionHeart?: boolean;
+  isMeReactionMusic?: boolean;
+
+  commentCount: number;
+  reactionCount: number;
+}
+
+export type ReactionType =
+  | 'FIRE'
+  | 'STAR'
+  | 'CONGRATULATIONS'
+  | 'HEART'
+  | 'MUSIC';
+
+export interface IReactionToggle {
+  reactionType: ReactionType;
+  count: number;
+}
+
+export interface IEmojiGroup {
+  fireCount: number;
+  starCount: number;
+  congratulationsCount: number;
+  heartCount: number;
+  musicCount: number;
+  isMeReactionFire: boolean;
+  isMeReactionStar: boolean;
+  isMeReactionCongratulations: boolean;
+  isMeReactionHeart: boolean;
+  isMeReactionMusic: boolean;
+  feedId: string;
+}
+
+export interface IEmojiItem {
+  emojiType: ReactionType;
+  count: number;
+  isClicked: boolean;
+  onClick: () => void;
+}
+
+export interface IActionIcons {
+  reactionCount: number;
+  commentCount: number;
 }
