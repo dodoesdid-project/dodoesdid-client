f2b408714afdc093628f49cac3f305c3c0525ed5
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-10-04 15:04:26 +0900

    chore: env -#54

--- src/lib/api/deafultAxios.ts
+++ src/lib/api/deafultAxios.ts
@@ -6,9 +6,3 @@ export const defaultAxios = axios.create({
   baseURL,
   withCredentials: true,
 });
-
-defaultAxios.interceptors.request.use(function (config) {
-  const accessToken = localStorage.getItem('authorization');
-  config.headers.Authorization = accessToken ? `Bearer ${accessToken}` : '';
-  return config;
-});
