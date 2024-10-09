f278ecdfc02615bf20900d6a8351c95990bde9c8
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-10-09 21:30:36 +0900

    refactor: api-spec -#74

--- src/lib/api/dazims.ts
+++ src/lib/api/dazims.ts
@@ -2,20 +2,24 @@ import { Dazims } from '../../types/dazims';
 import { defaultAxios } from './deafultAxios';
 import { AxiosResponse } from 'axios';
 
-// 다짐 리스트 조회
+// 다짐 리스트 조회 = 그룹회원전체조회(api)
 export const getDazims = async (
   groupId: string,
   date: string,
 ): Promise<AxiosResponse<Dazims>> => {
-  return await defaultAxios.get(`/api/v1/group/${groupId}/dazims?date=${date}`);
+  return await defaultAxios.get(
+    `/api/v1/groups/${groupId}/users?dazimCreateAt=${date}`,
+  );
 };
 
-// 다짐 글 등록
+// 다짐 글 등록 = 그룹다짐등록(api)
 export const createDazimTitle = async (data: {
   groupId: string;
   content: string;
 }) => {
-  await defaultAxios.post(`/api/v1/dazim`, data);
+  await defaultAxios.post(`/api/v1/groups/${data.groupId}/dazim`, {
+    content: data.content,
+  });
 };
 
 // 다짐 이미지 등록 = 다짐완료
