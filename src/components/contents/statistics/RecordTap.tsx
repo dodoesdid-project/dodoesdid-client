2b8e0741c42b5068161a568ab21841b5d8edd6ad
정찬영 <q135794268020@gmail.com>
2024-10-18 16:51:18 +0900

    feat: 통계 페이지 다크 모드 적용 -#90

--- src/components/contents/statistics/RecordTap.tsx
+++ src/components/contents/statistics/RecordTap.tsx
@@ -6,7 +6,7 @@ const RecordTap = ({ title, isActive, onClick }: ITap) => {
       <div className="flex justify-center text-center rounded-lg ">
         <button
           onClick={onClick}
-          className={`py-3 px-[52px]  rounded-lg w-full font-semibold ${isActive ? 'bg-white text-gray-100' : ' text-gray-70'}`}
+          className={`py-3 px-[52px] rounded-lg w-full font-semibold ${isActive ? 'bg-white text-gray-100 dark:bg-black dark:text-gray-30' : ' text-gray-70 dark:bg-[#2A2A2A] dark:text-gray-60'}`}
         >
           {title}
         </button>
